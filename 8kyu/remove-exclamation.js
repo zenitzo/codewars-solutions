0991ceb4e20dc5527a79870b2bc91921159baf75
zenitzo <christian.santos0327@gmail.com>
2023-05-21 15:14:13 -0400

    Second commit

--- 8kyu/remove-exclamation.js
+++ 8kyu/remove-exclamation.js
@@ -1,7 +1,8 @@
+//8KYU
+
 // Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
 
 //My solution:
-
 function removeExclamationMarks(s) {
   return s.replace(/!/gi, "");
 }
